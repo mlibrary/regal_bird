0603bb260c601ffd9774ba0d70cc4ed98366541c
Bryan Hockey <bhock@umich.edu>
2017-03-27 12:12:57 -0500

    spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "regal_bird"
+$: << File.expand_path(File.join(File.dirname(__FILE__), "../lib/regal_bird"))
+
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.disable_monkey_patching!
+  config.warnings = false
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+end
