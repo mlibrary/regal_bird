86a02f8030180b57814f41cbdc51ca9fed254f5b
Bryan Hockey <bhock@umich.edu>
2017-06-12 16:40:02 -0500

    Remove all repositories
    With the move to messaging, there's no longer a need for backing
    repositories for data or progress.
    
    With the move to a ruby DSL for defining plans, there's no longer
    a need for a plan repository.

--- spec/repositories/memory_repo_spec.rb
+++
@@ -1,7 +0,0 @@
-require "regal_bird/repositories/memory_repo"
-require_relative "../spec_helper"
-
-RSpec.describe RegalBird::MemoryRepo do
-  let(:repo) { described_class.new }
-  it_behaves_like "a PlanRepo"
-end
