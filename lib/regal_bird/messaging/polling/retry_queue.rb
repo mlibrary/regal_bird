e77c431737f2b37786522e6d45e526dbce6a81c7
Bryan Hockey <bhock@umich.edu>
2017-06-13 13:48:16 -0500

    Add Messaging::Polling::RetryQueue

---
+++ lib/regal_bird/messaging/polling/retry_queue.rb
@@ -0,0 +1,39 @@
+module RegalBird
+  module Messaging
+    module Polling
+
+      class RetryQueue
+
+        def initialize(channel, work_exchange, retry_exchange, step_class, interval)
+          @step_class = step_class
+          @queue = channel.queue(
+            name,
+            exclusive: false,
+            auto_delete: false,
+            durable: true,
+            arguments: {
+              "x-dead-letter-exchange" => work_exchange.name,
+              "x-message-ttl" => interval * 1000
+            }
+          )
+          @queue.bind(retry_exchange,
+            arguments: binding.merge({"x-match" => "all"})
+          )
+        end
+
+        def binding
+          { "source" => step_class.to_s }
+        end
+
+        private
+        attr_reader :step_class
+
+        def name
+          "source-#{step_class.to_s.downcase.gsub("::", "_")}-retry"
+        end
+
+      end
+
+    end
+  end
+end
---
+++ spec/messaging/polling/retry_queue_spec.rb
@@ -0,0 +1,40 @@
+require "regal_bird/messaging/polling/retry_queue"
+
+RSpec.describe RegalBird::Messaging::Polling::RetryQueue do
+  let(:queue) { double(:queue, bind: nil) }
+  let(:channel) { double(:channel, queue: queue) }
+  let(:work_exchange) { double(:work_exchange, name: "some_work_exchange") }
+  let(:retry_exchange) { double(:retry_exchange) }
+  let(:step_class) { described_class }
+  let(:routing_key) { "sometest.routing.key" }
+  let(:queue_name) { "source-regalbird_messaging_polling_retryqueue-retry" }
+  let(:interval) { 17 }
+  subject { described_class.new(channel, work_exchange, retry_exchange, step_class, interval) }
+
+  describe "#new" do
+    it "initializes a non-exclusive, durable, dlx queue" do
+      expect(channel).to receive(:queue)
+        .with(queue_name, {
+        exclusive: false, auto_delete: false, durable: true,
+        arguments: {
+          "x-dead-letter-exchange" => work_exchange.name,
+          "x-message-ttl" => interval * 1000
+        }
+      })
+      subject
+    end
+
+    it "binds the queue to the retry exchange on 'source' => step_class.to_s" do
+      expect(queue).to receive(:bind)
+        .with(retry_exchange, {arguments: {"x-match" => "all", "source" => step_class.to_s}})
+      subject
+    end
+  end
+
+  describe "#binding" do
+    it "binds on the source" do
+      expect(subject.binding).to eql({"source" => step_class.to_s})
+    end
+  end
+
+end
