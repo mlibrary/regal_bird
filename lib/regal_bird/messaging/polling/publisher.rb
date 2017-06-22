fd18ddd49cac3a5a679fc2fad9449b0b8e2a5d69
Bryan Hockey <bhock@umich.edu>
2017-06-22 13:02:19 -0500

    Add Messagin::Polling::Publisher

---
+++ lib/regal_bird/messaging/polling/publisher.rb
@@ -0,0 +1,34 @@
+module RegalBird
+  module Messaging
+    module Polling
+
+      class Publisher
+
+        def initialize(work_exchange, retry_exchange)
+          @work_exchange = work_exchange
+          @retry_exchange = retry_exchange
+        end
+
+        def retry(message)
+          retry_exchange.publish(
+            EventSerializer.serialize(message.event),
+            routing_key: message.routing_key,
+            headers: message.headers
+          )
+        end
+
+        def success(event)
+          work_exchange.publish(
+            EventSerializer.serialize(event),
+            routing_key: "action.#{event.state}"
+          )
+        end
+
+        private
+        attr_reader :work_exchange, :retry_exchange
+
+      end
+
+    end
+  end
+end
