15f558860c0164802a42dbd1e9f6355c4332202f
Bryan Hockey <bhock@umich.edu>
2017-06-23 11:57:57 -0500

    Add Messaging::Polling::Arrangement

---
+++ lib/regal_bird/messaging/polling/arrangement.rb
@@ -0,0 +1,44 @@
+require "regal_bird/event"
+require "regal_bird/messaging/message"
+require "regal_bird/messaging/polling/consumer"
+require "regal_bird/messaging/polling/publisher"
+require "regal_bird/messaging/polling/retry_queue"
+require "regal_bird/messaging/polling/work_queue"
+
+module RegalBird
+  module Messaging
+    module Polling
+
+      class Arrangement
+
+        # @param channel [Channel]
+        # @param work_exchange [WorkExchange]
+        # @param retry_exchange [RetryExchange]
+        # @param step_class [Step]
+        # @param interval [Fixnum] seconds
+        def initialize(channel, work_exchange, retry_exchange, step_class, interval)
+          @step_class = step_class
+          @retry_queue = RetryQueue.new(channel, work_exchange, retry_exchange, step_class, interval)
+          @work_queue = WorkQueue.new(channel, work_exchange, step_class, "source.#{step_class}")
+          @publisher = Publisher.new(work_exchange, retry_exchange)
+          @consumer = Consumer.new(@work_queue, @publisher, step_class)
+          @publisher.retry(initial_message)
+        end
+
+        private
+
+        def initial_message
+          Message.new({}, { headers: @retry_queue.binding }, RegalBird::Event.new(
+            item_id: @step_class.to_s,
+            emitter: @step_class,
+            state: :source,
+            data: {},
+            start_time: Time.at(0),
+            end_time: Time.at(0)
+          ))
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/messaging/polling/arrangement_spec.rb
@@ -0,0 +1,77 @@
+require "regal_bird/messaging/polling/arrangement"
+require "regal_bird/messaging/polling/consumer"
+require "regal_bird/messaging/polling/publisher"
+require "regal_bird/messaging/polling/retry_queue"
+require "regal_bird/messaging/polling/work_queue"
+
+RSpec.describe RegalBird::Messaging::Polling::Arrangement do
+  let(:channel) { double(:channel) }
+  let(:work_exchange) { double(:work_exchange) }
+  let(:retry_exchange) { double(:retry_exchange) }
+  let(:step_class) { Fixnum }
+  let(:interval) { 30 }
+  let(:arrangement) { described_class.new(channel, work_exchange, retry_exchange, step_class, interval) }
+
+  let(:consumer) { double(:consumer) }
+  let(:publisher) { double(:publisher, success: nil, retry: nil) }
+  let(:retry_queue) { double(:retry_queue, binding: "foo") }
+  let(:work_queue) { double(:work_queue) }
+
+  before(:each) do
+    allow(RegalBird::Messaging::Polling::Consumer).to receive(:new).and_return(consumer)
+    allow(RegalBird::Messaging::Polling::Publisher).to receive(:new).and_return(publisher)
+    allow(RegalBird::Messaging::Polling::RetryQueue).to receive(:new).and_return(retry_queue)
+    allow(RegalBird::Messaging::Polling::WorkQueue).to receive(:new).and_return(work_queue)
+  end
+
+  describe "::new" do
+    it "creates a Consumer" do
+      expect(RegalBird::Messaging::Polling::Consumer).to receive(:new).with(
+        work_queue, publisher, step_class
+      )
+      arrangement
+    end
+
+    it "creates a Publisher" do
+      expect(RegalBird::Messaging::Polling::Publisher).to receive(:new).with(
+        work_exchange, retry_exchange
+      )
+      arrangement
+    end
+
+    it "creates a RetryQueue" do
+      expect(RegalBird::Messaging::Polling::RetryQueue).to receive(:new).with(
+        channel, work_exchange, retry_exchange, step_class, interval
+      )
+      arrangement
+    end
+
+    it "creates a WorkQueue" do
+      expect(RegalBird::Messaging::Polling::WorkQueue).to receive(:new).with(
+        channel, work_exchange, step_class, "source.#{step_class.to_s}"
+      )
+      arrangement
+    end
+
+    it "retries the initial message" do
+      expect(publisher).to receive(:retry).with(
+        RegalBird::Messaging::Message.new(
+          Hash.new,
+          { headers: retry_queue.binding },
+          RegalBird::Event.new(
+            item_id: step_class.to_s,
+            emitter: step_class,
+            state: :source,
+            data: {},
+            start_time: Time.at(0),
+            end_time: Time.at(0)
+          )
+        )
+      )
+      arrangement
+    end
+
+
+
+  end
+end
