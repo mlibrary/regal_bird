c62554560eb3011e55804be166ee4f3dade0d6f2
Bryan Hockey <bhock@umich.edu>
2017-06-23 11:55:10 -0500

    Messaging::Message expects hashy args

--- lib/regal_bird/messaging/message.rb
+++ lib/regal_bird/messaging/message.rb
@@ -11,16 +11,24 @@ module RegalBird
         attr_reader :event
 
         def delivery_tag
-          delivery_info.delivery_tag
+          delivery_info[:delivery_tag] || "not_yet_delivered"
         end
 
         def headers
-          properties.headers || {}
+          properties[:headers] || {}
         end
 
         def routing_key
-          delivery_info.routing_key
+          delivery_info[:routing_key] || "unrouted"
         end
+
+        def eql?(other)
+          delivery_tag == other.delivery_tag &&
+            headers == other.headers &&
+            routing_key == other.routing_key &&
+            event == other.event
+        end
+        alias_method :==, :eql?
 
 
         private
